f36018e7d4c7aa79983bdbebb05c3aec765489a1
pank-su <frostkslo1@yandex.ru>
2023-10-14 15:16:51 +0300

    Парсинг большого ГУАПа

--- index.ts
+++ index.ts
@@ -1,32 +1,44 @@
 import axios from 'axios';
-import {load} from 'cheerio';
-import mammoth = require("mammoth");
+import {CheerioAPI, Element, load} from 'cheerio';
 import {writeFileSync} from 'fs';
 import {createClient} from '@supabase/supabase-js'
-
+import * as pdf from 'pdf-parse'
+import OpenAI from 'openai'
+import * as util from "util";
+import mammoth = require("mammoth");
 
 const supabaseUrl = 'https://fecnldjxpserceyiifwt.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
+const openAIKEY = process.env.OPEN_AI_KEY
+
+const openai = new OpenAI({
+    apiKey: openAIKEY,
+});
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+var groupDbId = 1
 
 console.log(supabase.schema('public'))
 
-test()
+main()
 
-
-async function test(){
-    let { data, error } = await supabase.from('group').select()
+async function test() {
+    let {data, error} = await supabase.from('group').select()
 
     if (error) console.error(error)
     else console.log(data)
 
 }
 
+const timeFSPO = ["9:20 - 10:55", "11:05 - 12:40", "13:20 - 14:55", "15:05 - 16:40"]
+
+
+
 //main()
 
 async function parseTeachersFromSUAI() {
     const url = "https://pro.guap.ru"
-    let response = await axios.get(url + "/professors?position=47&facultyWithChairs=389&subunit=0&fullname=&perPage=100")
+    let response = await axios.get(url + "/professors?position=0&facultyWithChairs=0&subunit=0&fullname=&perPage=2000")
     let $ = load(response.data)
     const urls = $('#external_professors > div > div.col-lg-9 > div > div > div > div > div > div:nth-child(2) > h5 > a')
     let id = 1
@@ -67,8 +79,34 @@ async function main() {
     await supabase.from('teacher').delete().neq('last_name', null)
     await supabase.from('group').delete().neq('group_name', null)
     await supabase.from('subject').delete().neq('subject_name', null)
-    await parseTeachersFromSUAI()
-    parseDocxFromVk();
+    // await parseTeachersFromSUAI()
+
+    await parseBigGuap()
+
+    // parseDocxFromVk();
+    // parsePDFWithChatGPT()
+}
+
+
+// Париснг pdf файла с помощью Chat GPT
+async function downloadPDF() {
+    const url = "https://new.guap.ru/fspo/uch"
+    let response = await axios.get(url)
+    let $ = load(response.data)
+
+    const pdfUrl = $("#tab_uch_1 > div:nth-child(4) > div:nth-child(1) > ul > li > a")
+    console.log(pdfUrl[0].attribs["href"])
+    response = await axios.get(pdfUrl[0].attribs["href"], {
+        responseType: "arraybuffer",
+    });
+    const data = await pdf(response.data)
+    console.log(data.text.indexOf("РАСПИСАНИЕ ГРУППЫ", 40))
+    const chatCompletion = await openai.chat.completions.create({
+        messages: [{role: 'user', content: 'Say this is a test'}],
+        model: 'gpt-3.5-turbo',
+    });
+    console.log(chatCompletion.choices)
+
 }
 
 // Функция для загрузки файла docx по URL
@@ -92,6 +130,168 @@ interface ScheduleItem {
 interface ScheduleGroup {
     numerator: ScheduleItem[];
     denominator: ScheduleItem[];
+}
+
+async function parseLessonElement(lessonElement: Element, timeRange: RegExpMatchArray, $: CheerioAPI, dayName: string) {
+    const lesson = {
+        timeStart: timeRange[1],
+        timeEnd: timeRange[2],
+        type: '',
+        subjects: [],
+    };
+
+    // Определите тип занятия
+    const text = $(lessonElement).next('div.study').text();
+    // lesson.type = lessonType;
+
+    let pattern = /.*[A-ZА-Я]+ \– ([A-Za-zА-Яа-я0-9ё \"\(\)\-,\.\:\;]{2,})  \– ([А-Яа-я0-9\. ]{2,}), ауд\. ([0-9\-а-я]{2,6}|спортзал)Преподаватель: ([А-Яа-я \.\-]{1,}) - .*/;
+
+    console.log(text)
+
+    if (text.includes("Преподаватели")) {
+        pattern = /.*[A-ZА-Я]+ \– ([A-Za-zА-Яа-я0-9ё \"\(\)\-,\.\:\;]{2,})  \– ([А-Яа-я0-9\. ]{2,}), ауд\. ([0-9\-а-я]{2,6}|спортзал)Преподаватели: ([А-Яа-я \.\-]{1,}) - .*/;
+    } else if (text.includes("спортзал") || !text.includes("Преподаватель")) {
+        pattern = /.*[A-ZА-Я]+ \– ([A-Za-zА-Яа-я0-9ё \"\(\)\-,\.\:\;]{2,})  \– ([А-Яа-я0-9\. ]{2,}), ауд\. ([0-9\-а-я]{2,6}|спортзал).*/;
+    }
+    let match = text.match(pattern);
+    if (match == null) {
+        pattern = /.*[A-ZА-Я]+ \– ([A-Za-zА-Яа-я0-9ё \"\(\)\-,\.\:\;]{2,})  \– ([А-Яа-я0-9\. ]{2,}), ауд\./;
+        match = text.match(pattern);
+    }
+    let isNumerator: boolean|null = null
+    const [, subject, location, auditorium, teacher] = match;
+    if (text.includes("▲") || text.includes("▼")){
+        isNumerator = text.includes("▲")
+    }
+
+
+    const {data} = await supabase.from('subject').select('id').eq('subject_name', subject)
+    let subjectIdForSchedule: number;
+    // Если предмета нет в базе, то добавляем
+    if (data.length == 0) {
+        await supabase.from('subject').insert({id: subjectId++, subject_name: subject})
+        subjectIdForSchedule = subjectId - 1
+    } else {
+        subjectIdForSchedule = data[0].id
+    }
+
+    if (isNumerator == null){
+        await supabase.from('schedule').insert({
+            id: curId++, // Id
+            group_id: groupDbId - 1, // id группы
+            subject_id: subjectIdForSchedule, // id предмета
+            time_str: `${timeRange[0]} - ${timeRange[1]}`, // номер предмета по расписанию
+            is_numerator: false, // это числитель?
+            day_id: dayIdFromName(dayName) // день недели
+        })
+        await supabase.from('schedule').insert({
+            id: curId++, // Id
+            group_id: groupDbId - 1, // id группы
+            subject_id: subjectIdForSchedule, // id предмета
+            time_str: `${timeRange[0]} - ${timeRange[1]}`, // номер предмета по расписанию
+            is_numerator: true, // это числитель?
+            day_id: dayIdFromName(dayName) // день недели
+        })
+    } else{
+        await supabase.from('schedule').insert({
+            id: curId++, // Id
+            group_id: groupDbId - 1, // id группы
+            subject_id: subjectIdForSchedule, // id предмета
+            time_str: `${timeRange[0]} - ${timeRange[1]}`, // номер предмета по расписанию
+            is_numerator: isNumerator, // это числитель?
+            day_id: dayIdFromName(dayName) // день недели
+        })
+    }
+    let department_id = 5
+    try {
+        let department_id = (await supabase.from("department").select("id").eq("address", location).single()).data.id
+    }catch (e) {
+
+    }
+
+    console.log(auditorium)
+
+    await supabase.from('cabinet').insert({
+        cabinet_number: auditorium,
+        department_id: department_id,
+    })
+
+    // const response = await supabase.rpc('find_teacher', {inicials: teacher.trim()})
+
+    console.log(teacher)
+    // console.log(response.data)
+    return lesson
+}
+
+function dayIdFromName(name: String){
+    return ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"].indexOf(name.trim())
+}
+
+async function parseBigGuap() {
+    const url = "https://guap.ru/rasp/"
+
+    for (let i = 1; i < 529; i++) {
+        let response = await axios.get(url + "?g=" + i)
+        let $ = load(response.data)
+
+        const schedule = {
+            group: '',
+            days: [],
+            numerator: false
+        };
+
+        const groupInfo = $('h2').text().match(/группы - ([0-9МКСАВИ]{1,})/i);
+        if (groupInfo) {
+            schedule.group = groupInfo[1]
+        }
+        await
+            supabase.from('group').insert({group_id: groupDbId++, group_name: schedule.group})
+        console.log(groupInfo)
+
+        $('h3').each((_, dayElement) => {
+
+            if ($(dayElement).text().includes("Вне сетки расписания")){
+                return;
+            }
+            const day = {
+                name: $(dayElement).text(),
+                lessons: [],
+            };
+
+
+            // Выберите все пары в текущем дне
+            $(dayElement)
+                .nextUntil('h3')
+                .filter('h4')
+                .each((_, lessonElement) => {
+                    const timeRange = $(lessonElement).text().match(/(\d+:\d+)–(\d+:\d+)/);
+                    if (timeRange) {
+                        parseLessonElement(lessonElement, timeRange, $, day.name).then(
+                            (lesson) => day.lessons.push(lesson)
+                        )
+
+                        // Выберите все предметы в текущей паре
+                        // $(lessonElement)
+                        //     .next('div.study')
+                        //     .find('span > b')
+                        //     .each((_, subjectElement) => {
+                        //         const subject = $(subjectElement).text();
+                        //         lesson.subjects.push(subject);
+                        //     });
+
+
+                    }
+                });
+
+            schedule.days.push(day);
+        })
+        //console.log(util.inspect(schedule, {showHidden: false, depth: null, colors: true}))
+        // $("#Form1 > div.page > div > div.result > h3:nth-child(7)")
+    }
+    while (groupDbId != 528){
+
+    }
+
 }
 
 function parseSchedule(data: string): Record<string, ScheduleGroup> {
@@ -185,7 +385,9 @@ async function parseAndAddToTables(schedule: ScheduleItem[], groupDbId: number, 
         let dayId = 0;
         for (const [key, value] of Object.entries(time)) {
             // Урока нет по расписанию
-            if (key == 'time' || value == "-------") {dayId++;continue;
+            if (key == 'time' || value == "-------") {
+                dayId++;
+                continue;
             }
             let sepCount = 0
 
@@ -222,7 +424,7 @@ async function parseAndAddToTables(schedule: ScheduleItem[], groupDbId: number, 
                 id: curId++, // Id
                 group_id: groupDbId - 1, // id группы
                 subject_id: subjectIdForSchedule, // id предмета
-                time_id: timeId, // номер предмета по расписанию
+                time_str: timeFSPO[timeId - 1], // номер предмета по расписанию
                 is_numerator: isNumerator, // это числитель?
                 day_id: dayId++ // день недели
             })
@@ -262,9 +464,10 @@ async function parseAndAddToTables(schedule: ScheduleItem[], groupDbId: number, 
     }
 }
 
+
 async function parseDocxFromVk() {
     // Получаем данные из вконтакте о группе
-    const groupId = 144922677;
+    const groupId = 222976377;
     const myToken = process.env.VK_TOKEN;
 
 
@@ -273,7 +476,7 @@ async function parseDocxFromVk() {
     // Получаем меню
     const groupMenu = response.data["response"][0]['menu'];
     // Получаем из меню ссылку на расписание
-    const urlToDocx = groupMenu["items"].filter(ob => ob.title == "Расписание")[0]["url"] + "&no_preview=1";
+    const urlToDocx = groupMenu["items"].filter(ob => ob.title == "Расписание")[0]["url"] + "?no_preview=1";
     // Преобразование docx to html
     let htmlRasp = await mammoth.convertToHtml({buffer: (await loadDocxFromUrl(urlToDocx))})
 
@@ -281,8 +484,10 @@ async function parseDocxFromVk() {
 
     const schedules = parseSchedule(htmlRasp.value);
 
+    console.log(util.inspect(schedules, {showHidden: false, depth: null, colors: true}))
+
     // indexes
-    let groupDbId = 1
+
 
     // Обход расписаний для каждой группы
     for (const groupName in schedules) {
@@ -290,9 +495,9 @@ async function parseDocxFromVk() {
             // Добавление группы
             await
                 supabase.from('group').insert({group_id: groupDbId++, group_name: groupName})
-            await parseAndAddToTables(schedules[groupName].numerator,  groupDbId, true);
+            await parseAndAddToTables(schedules[groupName].numerator, groupDbId, true);
             await parseAndAddToTables(schedules[groupName].denominator, groupDbId, false);
         }
     }
-    // console.log(parseSchedule(htmlRasp.value))
+    console.log(parseSchedule(htmlRasp.value))
 }
